16bdffc8dc5022ea3fe8afb050aaefeb99404c4a
jeromejguay <jerome.guay@protonmail.com>
2021-02-12 12:51:09 -0500

    new branche

--- viking_ADCP_processing/format_data/adcp_variable_format.py
+++ viking_ADCP_processing/format_data/adcp_variable_format.py
@@ -9,3 +9,8 @@ Instead, you could just import the variables needed.
 - TODO netcdf4 exporter (Empty Variables Shells).
 - TODO csv exporter.
 """
+
+
+
+
+fn = '/home/jeromeguay/Workspace/Project/pycurrents_ADCP_processing/ncdata/a1_20050503_20050504_0221m.adcp.L1.nc'
