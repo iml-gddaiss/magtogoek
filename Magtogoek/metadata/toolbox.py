feafa6d0cf19049023f2730be3b82b1e1db2d789
jeromejguay <jerome.guay@protonmail.com>
2021-02-19 12:21:38 -0500

    dank restructuring

---
+++ Magtogoek/metadata/toolbox.py
@@ -0,0 +1,30 @@
+"""
+author: Jérôme Guay
+date: Feb. 19, 2021
+
+Toolbox for metadata-subpackage
+"""
+import typing as tp
+import json
+
+# --------------------------------------------------------------------------- #
+# ------------------- Making of the json attributes file -------------------- #
+# --------------------------------------------------------------------------- #
+
+
+def dict2json(file_name: str, dictionnary: tp.Dict, indent: int = 4) -> None:
+    """Makes json file from dictionnary
+    Parameters:
+    -----------
+        indent:
+            argument is passed to json.dump(..., indent=indent)
+    """
+    with open(file_name, "w") as f:
+        json.dump(dictionnary, f, indent=indent)
+
+
+def json2dict(json_file: str):
+    """Open json file as a dictionnary."""
+    with open(json_file) as f:
+        dictionary = json.load(f)
+    return dictionary
