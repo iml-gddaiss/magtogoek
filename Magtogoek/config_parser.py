57f128aab95467c7e52b64c5c66cb11e82161137
jeromejguay <jerome.guay@protonmail.com>
2021-02-23 18:26:11 -0500

    dank commit

--- Magtogoek/config_parser.py
+++ Magtogoek/config_parser.py
@@ -1,26 +1,74 @@
+#!/usr/bin/python
 """
 author: Jérôme Guay
-
 date: Feb. 22, 2021
-TODO rename to templates.
-TODO move adcp section to adcp.
+
+File to be run in command line:
+
+$: config_parser filename sensor_type
+
+[-h] for help.
+
+TODO move adcp section to adcp sub-module.
+
 """
-import configparser as cp
+import configparser
+import argparse
 import typing as tp
 import pandas as pd
 import getpass
+
+if __name__ == "__main__":
+    from config_parser import make_config_file  # probably not good practice TODO
+
+    parser = argparse.ArgumentParser(usage=__doc__)
+
+    parser.add_argument(
+        "process",
+        metavar="1 - process",
+        help="""str: Type of the data to process.
+        Must be of one: `adcp`""",
+    )
+    parser.add_argument(
+        "filename",
+        metavar="2 - filename",
+        default="process_config",
+        help="""str: name of the config file.
+        The config file is place is current the
+        directory unless a path is provided.""",
+    )
+    args = parser.parse_args()
+
+    # remove any extension and check for multiple `.`.
+    filename = args.filename.split(".")
+    if len(filename) > 2:
+        raise parser.error("filename invalid, more than one `.`")
+    else:
+        filename = filename[0]
+
+    # verify if process is valid.
+    valid_process = ["adcp"]
+    if args.process not in valid_process:
+        raise parser.error("Invalid process. Must be one of : `adcp`")
+
+    make_config_file(filename + ".ini", sensor_type=args.process)
+    print(f"Config file created for {args.process}  -> {filename}.ini")
 
 
 def make_config_file(filename: str, sensor_type: str) -> None:
     """make empty config_files in local directory"""
     linewidth = 70
+
     config = _init_config(sensor_type, linewidth)
     _input_files(config, linewidth)
     _output_files(config, linewidth)
+
     if sensor_type == "adcp":
         _adcp_config(config, linewidth)
     else:
-        print("Invalid sensor_type. Support: adcp")
+        print("Invalid sensor_type. Must be one of : `adcp`")
+        exit()
+
     _project(config, linewidth)
     _cruise(config, linewidth)
     _netcdf_cf(config, linewidth)
@@ -30,11 +78,13 @@ def make_config_file(filename: str, sensor_type: str) -> None:
     configparser2ini(config, filename)
 
 
-def _init_config(sensor_type: str, linewidth: int) -> tp.Type[cp.ConfigParser]:
+def _init_config(
+    sensor_type: str, linewidth: int
+) -> tp.Type[configparser.ConfigParser]:
     """init configparser"""
     date = pd.Timestamp.now().strftime("%Y-%m-%d")
     user = getpass.getuser()
-    config = cp.ConfigParser(allow_no_value=True)
+    config = configparser.ConfigParser(allow_no_value=True)
     config.optionxform = str
     config["HEADER"] = {
         ";#".ljust(linewidth, "-") + "#": None,
@@ -49,7 +99,7 @@ def _init_config(sensor_type: str, linewidth: int) -> tp.Type[cp.ConfigParser]:
     return config
 
 
-def _input_files(config: tp.Type[cp.ConfigParser], linewidth: int):
+def _input_files(config: tp.Type[configparser.ConfigParser], linewidth: int):
     config["INPUT"] = {
         ";#".ljust(linewidth, "-") + "#": None,
         ";| data_file: (file name) Data to be process.".ljust(linewidth, " ")
@@ -62,17 +112,17 @@ def _input_files(config: tp.Type[cp.ConfigParser], linewidth: int):
     }
 
 
-def _output_files(config: tp.Type[cp.ConfigParser], linewidth: int):
+def _output_files(config: tp.Type[configparser.ConfigParser], linewidth: int):
     config["OUTPUT"] = {
         ";#".ljust(linewidth, "-") + "#": None,
-        "; (files name) Leave blank for False.".ljust(linewidth, " ") + "|": None,
+        "; (files name) Leave blank for `False`.".ljust(linewidth, " ") + "|": None,
         ";#".ljust(linewidth, "-") + "# ": None,
         "\t netcdf_output": "",
         "\t odf_output": "",
     }
 
 
-def _netcdf_cf(config: tp.Type[cp.ConfigParser], linewidth: int):
+def _netcdf_cf(config: tp.Type[configparser.ConfigParser], linewidth: int):
     """add netcdf config"""
     config["NETCDF_CF"] = {
         ";#".ljust(linewidth, "-") + "#": None,
@@ -83,13 +133,13 @@ def _netcdf_cf(config: tp.Type[cp.ConfigParser], linewidth: int):
         "\t title": "",
         "\t institution": "",
         "\t summary": "",
-        "\t references": "https://github.com/JeromeJGuay/Magtogoek",
+        "\t references": "https://github.com/JeromeJGuay/magtogoek",
         "\t comments": "",
         "\t naming_authority": "BODC, SDC, CF, MEDS",
     }
 
 
-def _project(config: tp.Type[cp.ConfigParser], linewidth: int):
+def _project(config: tp.Type[configparser.ConfigParser], linewidth: int):
     config["PROJECT"] = {
         ";#".ljust(linewidth, "-") + "#": None,
         ";| Global attributes for project".ljust(linewidth, " ") + "|": None,
@@ -101,7 +151,7 @@ def _project(config: tp.Type[cp.ConfigParser], linewidth: int):
     }
 
 
-def _cruise(config: tp.Type[cp.ConfigParser], linewidth: int):
+def _cruise(config: tp.Type[configparser.ConfigParser], linewidth: int):
     config["CRUISE"] = {
         ";#".ljust(linewidth, "-") + "#": None,
         ";| Global attributes for cruise".ljust(linewidth, " ") + "|": None,
@@ -116,12 +166,12 @@ def _cruise(config: tp.Type[cp.ConfigParser], linewidth: int):
         "\t cruise_number": "",
         "\t organization": "",
         "\t chief_scientist": "",
-        "\t start_date": ")",
+        "\t start_date": "",
         "\t end_date": "",
     }
 
 
-def _gloabal_attributes(config: tp.Type[cp.ConfigParser], linewidth: int):
+def _gloabal_attributes(config: tp.Type[configparser.ConfigParser], linewidth: int):
     config["GLOBAL_ATTRIBUTES"] = {
         ";#".ljust(linewidth, "-") + "#": None,
         ";| Global attributes ".ljust(linewidth, " ") + "|": None,
@@ -141,7 +191,9 @@ def _gloabal_attributes(config: tp.Type[cp.ConfigParser], linewidth: int):
     }
 
 
-def _additional_global_attributes(config: tp.Type[cp.ConfigParser], linewidth: int):
+def _additional_global_attributes(
+    config: tp.Type[configparser.ConfigParser], linewidth: int
+):
     config["ADDITIONAL_GLOBAL_ATTRIBUTES"] = {
         ";#".ljust(linewidth, "-") + "#": None,
         ";| Insert addittional attributes below.".ljust(linewidth, " ") + "|": None,
@@ -149,19 +201,22 @@ def _additional_global_attributes(config: tp.Type[cp.ConfigParser], linewidth: i
     }
 
 
-def _adcp_config(config: tp.Type[cp.ConfigParser], linewidth: int):
+def _adcp_config(config: tp.Type[configparser.ConfigParser], linewidth: int):
     """set PROCESSING for adcp"""
     config["ADCP_PROCESSING"] = {
         ";#".ljust(linewidth, "-") + "#": None,
-        ";| yearbase: year that the sampling started. ex: 1970".ljust(linewidth, " ")
+        ";| yearbase: year that the sampling started. ex: `1970`".ljust(linewidth, " ")
         + "|": None,
-        ";| adcp_orientation: down or up. (horizontal no supported)".ljust(
+        ";| adcp_orientation: `down` or `up`. (horizontal no supported)".ljust(
             linewidth, " "
         )
+        + "|": None,
+        ";| sonar:  Must be one of `wh`, `os`, `bb`, `nb` or `sw".ljust(linewidth, " ")
         + "|": None,
         ";#".ljust(linewidth, "-") + "# ": None,
         "\t yearbase": "",
         "\t adcp_orientation": "",
+        "\t sonar": "",
     }
     config["ADCP_QUALITY_CONTROL"] = {
         ";#".ljust(linewidth, "-") + "#": None,
@@ -192,7 +247,7 @@ def _adcp_config(config: tp.Type[cp.ConfigParser], linewidth: int):
     return config
 
 
-def configparser2ini(config: tp.Type[cp.ConfigParser], filename: str):
+def configparser2ini(config: tp.Type[configparser.ConfigParser], filename: str):
     """make ini file"""
     with open(filename, "w") as configfile:
         config.write(configfile)
