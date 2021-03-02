2be5982875744a90c63d66cf63a5006077ea3dc9
jeromejguay <jerome.guay@protonmail.com>
2021-03-02 13:30:38 -0500

    setup upadated to have console script

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="magtogoek",
     version="0.0.1",
-    author="jerome guay,",
+    author="JeromeJGuay,",
     author_email="jerome.guay@dfo-mpo.gc.ca",
-    description="""This package aim to process ocean data from the different
-    instruments. Magtogoek is the Algonquin name for the Saint-Lawrence River which mean "the path that walks.""",
+    description="""
+    This package aim to process ocean data from the different instruments.
+    Magtogoek is the Algonquin name for the Saint-Lawrence River which mean
+    'the path that walks.'""",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JeromeJGuay/magtogoek",
@@ -25,9 +27,16 @@ setuptools.setup(
         #'shapely',
         "typing",
         "nptyping",
+        "click",
     ],
     packages=setuptools.find_packages(),
     package_data={"Magtogoek": ["*.geojson"]},
     classifiers=["Programming Language :: Python :: 3"],
     python_requires="~=3.7",
+    entry_points={
+        "console_scripts": [
+            "mag=magtogoek.bin.dev_click:main",
+            "mag_config=magtogoek.magtogoek_config:main",
+        ]
+    },
 )
