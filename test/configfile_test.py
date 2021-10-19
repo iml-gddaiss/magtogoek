66c5dc6f2284977ab28f3cb934751004726d8d3f
jeromejguay <jerome.guay@protonmail.com>
2021-10-19 16:21:23 -0400

    fixing typo

---
+++ test/configfile_test.py
@@ -0,0 +1,23 @@
+import pytest
+from magtogoek.configfile import (ConfigFileError, load_configfile,
+                                  make_configfile)
+
+
+def test_load():
+    load_configfile("data/config_test_files/valid_config.ini")
+
+
+@pytest.mark.parametrize(
+    "filename, error",
+    [
+        ("data/config_test_files/choice_error.ini", "choice"),
+        ("data/config_test_files/range_error.ini", "range"),
+        ("data/config_test_files/nargs_error.ini", "nargs"),
+        ("data/config_test_files/dtype_error.ini", "dtype"),
+        ("data/config_test_files/sensor_type_error.ini", "sensor_type"),
+    ],
+)
+def test_ConfigFileError(filename, error):
+    with pytest.raises(ConfigFileError) as excinfo:
+        load_configfile(filename)
+    assert str(excinfo.value.error) == error
