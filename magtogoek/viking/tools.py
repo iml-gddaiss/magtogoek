4b4f6a7999db0f439d5a35380dec4a43bc2c2c42
jeromejguay <jerome.guay@protonmail.com>
2022-03-16 19:35:24 -0400

    adding tools.py

--- magtogoek/viking/tools.py
+++ magtogoek/viking/tools.py
@@ -13,6 +13,8 @@ import xarray as xr
 
 GAS_CONSTANT = 8.31446261815324
 
+FARADAY_CONSTANT = 96485.365
+
 RINKO_COEFFS_KEYS = ('c0', 'c1', 'c2', 'd0', 'cp',
                      'b0', 'b1', 'b2', 'b3', 'b4',
                      'd1', 'd2')
@@ -92,13 +94,20 @@ def compensation_pres_temp_psal_rinko(doxy: np.ndarray,
     return doxy_sc * 44.66  # uM -> ml/L
 
 
-def vFET_from_pHEXT():
+def pHEXT_from_vFET():
+    """(Johnson et al. 2016)
+
+    pH_EXT = (V_EXT - k0_EXT - k2_EXT * temp) / S_nernst
+             + log(Cl_T)
+             + 2*log(Y_HCl)_T
+             - log(1 - S_T/K_S)
+             - log((1000 - 1.005 * psal) / 1000))
+
+    S_nernst = (R * T * ln(10)) / F
+    F : Faraday Constant
+    K_S :
     """
 
-    Returns
-    -------
-
-    """
 
 
 def total_chloride_in_seawater(psal: np.ndarray) -> np.ndarray:
@@ -139,6 +148,7 @@ def debye_huckel_HCl_activity_constant(temp: np.ndarray) -> np.ndarray:
     """(Khoo et al. 1977)
 
     A_DH = 0.0000034286 * temp**2 + 0.00067524 * temp + 0.49172143
+
     Parameters
     ----------
     temp :
@@ -146,6 +156,55 @@ def debye_huckel_HCl_activity_constant(temp: np.ndarray) -> np.ndarray:
 
     """
     return 0.0000034286 * temp**2 + 0.00067524 * temp + 0.49172143
+
+
+def total_sulfate_in_seawater(psal: np.ndarray) -> np.ndarray:
+    """(Dickson et al. 2007)
+
+    S_T = (0.1400 / 96.062) * (psal / 1.80655)
+
+    Parameters
+    ----------
+    psal :
+        practical salinity
+
+    Notes
+    -----
+        Dickson et al. 2007, IOCCP Report No.8, Guide to Best Practices for Ocean CO2 Measurements
+    """
+
+    return (0.1400 / 96.062) * (psal / 1.80655)
+
+
+def partial_molal_volume_hcl(temp: np.ndarray)->np.ndarray:
+    """(Millero 1983)
+
+    V_HCl = 17.85 + 0.1044 * temp - 0.001316 * temp**2
+
+    Parameters
+    ----------
+    temp :
+        temperature in Celsius
+
+    """
+    return 17.85 + 0.1044 * temp - 0.001316 * temp**2
+
+
+def acid_dissociation_constant_HSO4(temp: np.ndarray, psal: np.ndarray) -> np.ndarray:
+    """(Dickson et al. 2007)
+
+    K_S = (1 - 0.001005 * psal)*e**Term
+    Term = (-4276.1 / T) + 141.328 - 23.093 * ln(T) + sqrt(I) * ((-13856/T) + 324.57 - 47.986*ln(T)) + ....
+
+    Parameters
+    ----------
+    psal :
+        practical salinity
+    temp :
+        temperature in Celsius
+
+
+    """
 
 
 def log_of_HCl_activity_as_temperature_function(psal: np.temp, temp: np.ndarray) -> np.ndarray:
@@ -190,27 +249,12 @@ def log_of_HCl_activity_as_temperature_and_pressure_function(psal: np.temp,
         practical salinity
     temp :
         temperature in Celsius
+    pres :
+        pressure in dbar
     """
     temp_k = 273.15 + temp
     log_t = log_of_HCl_activity_as_temperature_function(psal=psal, temp=temp)
-    v_hcl = None # FIXME
+    v_hcl = partial_molal_volume_hcl(temp=temp)
 
     return log_t + ((v_hcl * pres) / (np.log(10) * GAS_CONSTANT * temp * 10)) / 2
 
-
-def total_sulfate_in_seawater(psal: np.ndarray) -> np.ndarray:
-    """(Dickson et al. 2007)
-
-    S_T = (0.1400 / 96.062) * (psal / 1.80655)
-
-    Parameters
-    ----------
-    psal :
-        practical salinity
-
-    Notes
-    -----
-        Dickson et al. 2007, IOCCP Report No.8, Guide to Best Practices for Ocean CO2 Measurements
-
-
-    """
