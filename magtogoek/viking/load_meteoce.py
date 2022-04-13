71774bd3bd5403492e5045efe13ea0240bf25e61
jeromejguay <jerome.guay@protonmail.com>
2022-04-13 16:36:15 -0400

    working on meteoc mapping

--- magtogoek/viking/load_meteoce.py
+++ magtogoek/viking/load_meteoce.py
@@ -5,13 +5,14 @@ This script is used to decode the data send by Viking as of February 2022 format
 
 Data That Need Processing
 -------------------------
+    co2_a and co2_w: ppm values are divided by 1e6 and multiplied byt the cell_gas_pressure.
+
     WpH -> Algorithm in magtogoek/viking/tools.py
 
 
     - BODC NAMES: https://vocab.nerc.ac.uk/search_nvs/P01/
 
-    - METEOCE BODC:
-
+METEOCE BODC:
         Time                                                 : SYTM_01 : SDN:P01::
         Longitude (East +ve)                                 : LOND_01 : SDN:P01::
         Latitude (North +ve)                                 : LATD_01 : SDN:P01::
@@ -45,11 +46,14 @@ Data That Need Processing
         Hmax2                               : SDN:P01::
         Pmax                                : SDN:P01::
 
+    - VARIABLES NOT IN METEOCE odf.
+      PAR
+      chlorophyle
+      fdom
+
     - DO computation:
         https://github.com/TEOS-10/python-gsw/blob/master/gsw/gibbs/conversions.py
         https://github.com/ooici/ion-functions/blob/master/ion_functions/data/do2_functions.py
-
-    - Everything is loaded as float since nans are not available for int type in numpy
 
 """
 from magtogoek.viking.raw_dat_reader import VikingReader, VikingData
@@ -91,7 +95,7 @@ meteoc_variables = {
 }
 
 VARIABLES_UNITS = {
-    'par_digi': {'PAR': 'micromoles photons per meter squared per second'}
+    'par_digi': {'PAR': 'micromoles photons per meter squared per second'},
     'triplet': {'fluo': 'milligramme per meter squared'},
     'gps': {'speed': 'meters per second'},
     'rti': {'position': 'meters',
@@ -106,93 +110,116 @@ VARIABLES_UNITS = {
 
 def make_meteoce_dataset(viking_data: VikingData) -> xr.Dataset:
     _coords = {'time': np.asarray(viking_data.time)}
-    _data = {'lon': viking_data.longitude,
-             'lat': viking_data.latitude,
+    _data = {'lon': (viking_data.longitude, {}),
+             'lat': (viking_data.latitude, {}),
              }
+    _global_attrs = {'buoy_name': viking_data.buoy_name,
+                     'firmware': viking_data.firmware,
+                     'controller_serial_number': viking_data.controller_sn}
+
+    if viking_data.comp is not None:
+        _data.update(
+            {'heading': (viking_data.comp['heading'], {}),
+             'pitch': (viking_data.comp['pitch'], {}),
+             '_roll': (viking_data.comp['roll'], {}),
+             'tilt': (viking_data.comp['tilt'], {})}
+        )
 
     if viking_data.wmt700 is not None:
         _data.update(
-            {'wind_mean': viking_data.wmt700['Sm'],
-             'wind_direction_mean': viking_data.wmt700['Dm'],
-             'wind_max': viking_data.wmt700['Sx'],
-             'wind_direction_max': viking_data.wmt700['Dx']}
+            {'wind_mean': (viking_data.wmt700['Sm'], {}),
+             'wind_direction_mean': (viking_data.wmt700['Dm'], {}),
+             'wind_max': (viking_data.wmt700['Sx'], {}),
+             'wind_direction_max': (viking_data.wmt700['Dx'], {})}
         )
     if viking_data.wxt520 is not None:
         _data.update(
-            {'atm_temperature': viking_data.wxt520['Ta'],
-             'atm_humidity': viking_data.wxt520['Ua'],
-             'atm_pressure': viking_data.wxt520['Pa']}
+            {'atm_temperature': (viking_data.wxt520['Ta'], {}),
+             'atm_humidity': (viking_data.wxt520['Ua'], {}),
+             'atm_pressure': (viking_data.wxt520['Pa'], {})}
         )
-        for key, value in {'wind_mean': 'Sm', 'wind_direction_mean': 'Dm',
-                           'wind_max': 'Sx', 'wind_direction_max': 'Dx'}:
-            if key in _data:
-                if not (~_data[key].mask).any():
-                    _data[key] = viking_data.wxt520[value]
+        for nc_name, viking_name in zip(('wind_mean', 'wind_direction_mean', 'wind_max', 'wind_direction_max'),
+                                        ('Sm', 'Dm', 'Sx', 'Dx')):
+            if nc_name in _data:
+                if not (~_data[nc_name][0].mask).any():
+                    _data[nc_name] = (viking_data.wxt520[viking_name], {})
+            else:
+                _data[nc_name] = (viking_data.wxt520[viking_name], {})
 
     if viking_data.ctd is not None:
         _data.update(
-            {'temperature': viking_data.ctd['temperature'],
-             'salinity': viking_data.ctd['salinity'],
-             'oxygen': viking_data.ctd['oxygen'],
-             'density': viking_data.ctd['density']}
-        )# TODO needs to be computed
+            {'temperature': (viking_data.ctd['temperature'], {}),
+             'salinity': (viking_data.ctd['salinity'], {}),
+             'density': (viking_data.ctd['density'], {})}
+        )
 
     elif viking_data.ctdo is not None:
         _data.update(
-            {'temperature': viking_data.ctdo['temperature'],
-             'salinity': viking_data.ctdo['salinity'],
-             'oxygen': viking_data.ctd['oxygen']}
+            {'temperature': (viking_data.ctdo['temperature'], {}),
+             'salinity': (viking_data.ctdo['salinity'], {}),
+             'oxygen': (viking_data.ctd['oxygen'], {})}
         )
          # TODO 'density' needs to be computed
 
     if viking_data.wph is not None:
+        _attrs = {
+            'serial_number': viking_data.wph['serial_number'][~viking_data.wph['serial_number'].mask][0],
+            'model_number': viking_data.wph['model'][~viking_data.wph['model'].mask][0]
+        }
         _data.update(
-            {'ph': viking_data.wph['ph']}
+            {'ph': (viking_data.wph['ext_ph'], _attrs)}
         )# TODO needs to be corrected
 
     if viking_data.triplet is not None:
+        _attrs = {'serial_number': viking_data.triplet['serial_number'][~viking_data.triplet['serial_number'].mask][0],
+                  'model_number': viking_data.triplet['model_number'][~viking_data.triplet['model_number'].mask][0]}
         _data.update(
-            {'fluorescence': viking_data.triplet['fluo_calculated']}
+            {'fluo': (viking_data.triplet['fluo_calculated'], _attrs),
+             'chloro': (viking_data.triplet['chloro_calculated'], _attrs),
+             'fdom': (viking_data.triplet['fdom_calculated'], _attrs)}
         )
 
+    if viking_data.par_digi is not None:
+        _attrs = {
+            'serial_number': viking_data.par_digi['serial_number'][~viking_data.par_digi['serial_number'].mask][0],
+            'model_number': viking_data.par_digi['model_number'][~viking_data.par_digi['model_number'].mask][0]
+        }
+        _data['par'] = (viking_data.par_digi['PAR'], _attrs)
+
     if viking_data.co2_a is not None:
-        _data.update({})
+        _data.update({'co2_a': (viking_data.co2_a['cell_gas_pressure_mbar'] * viking_data.co2_a['co2_ppm'] / 1e6, {})})
 
     if viking_data.co2_w is not None:
-        _data.update({})
+        _data.update({'co2_w': (viking_data.co2_w['cell_gas_pressure_mbar'] * viking_data.co2_w['co2_ppm'] / 1e6 ,{})})
 
     if viking_data.wave_m is not None:
-        _data.update({})
+        _data.update(
+            {'average_height': (viking_data.wave_m['average_height'], {}),
+             'maximal_height': (viking_data.wave_m['maximal_height'], {}),
+             'wave_period': (viking_data.wave_m['period'], {})}
+        )
 
     elif viking_data.wave_s is not None:
-        _data.update({})
+        _data.update(
+            {'average_height': (viking_data.wave_s['average_height'], {}),
+             'maximal_height': (viking_data.wave_s['Hmax'], {}),
+             'wave_period': (viking_data.wave_s['dominant_period'], {})}
+        )
 
     if viking_data.rdi is not None:
-        _data.update({})
+        # TODO carry one quality control
+        for _name in ['u', 'v', 'w', 'e']:
+            _data[_name] = (viking_data.rdi[_name], {})
 
     elif viking_data.rti is not None:
-        _data.update({})
+        # TODO carry one quality control
+        _attrs = {'bin': viking_data.rti['bin'][~viking_data.rti['bin'].mask],
+                  'position': viking_data.rti['position_cm'][~viking_data.rti['position_cm'].mask].to('m')}
+        for _name in ['u', 'v', 'w', 'e', 'corr1', 'corr2', 'corr3', 'corr4', 'amp1', 'amp2', 'amp3', 'amp4']:
+            _data[_name] = (viking_data.rti[_name], _attrs)
+            _data["bt_"+_name] = (viking_data.rti["bt_"+_name], _attrs)
 
-
-
-
-def _get_wind_data(viking_data:VikingData)->dict:
-    data_map = {'wind_mean': [('wmt700', 'Sm'), ('wxt520', 'Sm')],
-                'wind_direction_mean': [('wmt700', 'Dm'), ('wxt520', 'Dm')],
-                'wind_max': [('wmt700', 'Sx'), ('wxt520', 'Sx')],
-                'wind_direction_max': [('wmt700', 'Sx'), ('wxt520', 'Sx')]}
-    _data = dict().fromkeys(data_map.keys())
-    for key, value in data_map.items():
-        for (tag, varname) in value:
-            if viking_data.__dict__[tag]
-            if (~viking_data.__dict__[tag][varname].mask).any():
-                _data[key] = viking_data.__dict__[tag][varname]
     return _data
-
-
-
-def _get_atmospheric_data(viking_data:VikingData)->dict:
-
 
 
 def _average_duplicates(dataset: xr.Dataset, coord: str) -> xr.Dataset:
@@ -207,3 +234,4 @@ def _average_duplicates(dataset: xr.Dataset, coord: str) -> xr.Dataset:
         _dataset[var].values[:] = df[var][:]
 
     return _dataset
+
