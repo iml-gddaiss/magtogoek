17565ff8a1b4e3b1c79a3cf34284e9cb1ef3f7da
jeromejguay <jerome.guay@protonmail.com>
2022-11-03 15:51:42 -0400

    adding coord transform function

--- magtogoek/adcp/transform.py
+++ magtogoek/adcp/transform.py
@@ -1,82 +1,94 @@
 """Module that contains transformation function for adcp data.
+TODO
+----
+-> test if it works or if you need masked arrays.
 
 Notes
 -----
 Move Coordinate transformation here
 Move motion correction to here.
 """
+from typing import *
 import numpy as np
 import xarray as xr
 from pycurrents.adcp import transform
 
 
-def coordsystem2earth(dataset: xr.Dataset, orientation: str):
+def coordsystem2earth(dataset: xr.Dataset, vels: Tuple[str] = None):
     """Transforms beam and xyz coordinates to enu coordinates
 
-    NOTE: not properly tested. But it should work.
+        FIXME
 
-    Replace the values of data.vel, data.bt_vel with East, North and Up velocities
-    and the velocity error for 4 beams ADCP. UHDAS transform functions are used to
-    transform for beam coordinates and xyz to east-north-up (enu). These function
-    can use a three-beam solution by faking a fourth beam.
+        Replace the values of data.vel, data.bt_vel with East, North and Up velocities
+        and the velocity error for 4 beams ADCP. UHDAS transform functions are used to
+        transform for beam coordinates and xyz to east-north-up (enu). These function
+        can use a three-beam solution by faking a fourth beam.
 
-    Also change the values of of `coordinates` in data.trans.
+        Also change the values of of `coordinates` in data.trans.
 
-    beam coordinates : Velocity measured along beam axis.
-    xyz coordinates : Velocity in a cartesian coordinate system in the ADCP frame of reference.
-    enu coordinates : East North Up measured using the heading, pitch, roll of the ADCP.
+        beam coordinates : Velocity measured along beam axis.
+        xyz coordinates : Velocity in a cartesian coordinate system in the ADCP frame of reference.
+        enu coordinates : East North Up measured using the heading, pitch, roll of the ADCP.
+        """
+    if vels is None:
+        vels = ('u', 'v', 'w', 'e')
+
+    bt_vels = ('bt_' + v for v in vels)
+
+    if dataset.attrs["coord_system"] == "beam" and dataset.attrs["beam_angle"] is not None:
+        _beam2xyze(dataset, vels)
+        if all(v in dataset for v in bt_vels):
+            _beam2xyze(dataset, bt_vels)
+        dataset.attrs["coord_system"] = "xyz"
+
+    if any((dataset['heading'] == 0).all(),
+           (dataset['roll_'] == 0).all(),
+           (dataset['pitch'] == 0).all()):
+        _xyze2enu(dataset, vels)
+        if all(v in dataset for v in bt_vels):
+            _xyze2enu(dataset, vels)
+        dataset.attrs["coord_system"] = "earth"
+
+
+def _beam2xyze(dataset: xr.Dataset, vels: Tuple[str]):
+    """
 
     Parameters
     ----------
-    data:
-        pycurrents.adcp.rdiraw.Bunche object containing: vel[time, depth, beams], bt_vel[time, beams],
-        heading, roll, pitch sysconfig.convex, sysconfig.angle  and trans.coordsystem.
+    dataset
+    vels
 
-    orientation:
-        adcp orientation. Either `up` or `down`.
-    Notes
-    -----
-    Move the prints outside
+    Returns
+    -------
+
+    """
+    trans = transform.Transform(angle=dataset.attrs["beam_angle"], geometry=dataset.attrs["beam_pattern"])
+    xyze = trans.beam_to_xyz(np.stack([dataset[v].T for v in vels], axis=2))
+    for i, v in enumerate(vels):
+        dataset[v].values = np.round(xyze[:, :, i].T, decimals=3)
+
+
+def _xyze2enu(dataset: xr.Dataset, vels: Tuple[str]):
     """
 
-    if dataset.attrs["coord_system"] not in ["beam", "xyz"]:
-        pass
-        # l.log(f"Coordsystem value of {dataset.attrs['coord_system']} not recognized. Conversion to enu not available.")
+    Parameters
+    ----------
+    dataset
+    vels
 
-    beam_pattern = dataset.attrs["beam_pattern"]
+    Returns
+    -------
 
-    xyze, bt_xyze = data.vel, data.bt_vel
-
-    if data.trans.coordsystem == "beam":
-        if data.sysconfig.angle:
-            trans = transform.Transform(
-                angle=data.sysconfig.angle, geometry=beam_pattern
-            )
-            xyze = trans.beam_to_xyz(data.vel)
-            bt_xyze = trans.beam_to_xyz(data.bt_vel)
-        else:
-            pass
-            # l.log("Beam angle missing. Could not convert from beam coordinate.")
-
-    if (data.heading == 0).all() or (dataset['roll_'] == 0).all() or (dataset['pitch'] == 0).all():
-        dataset.attrs["coord_system"] = "xyz"
-
-        for i in range(4):
-            data.vel[:, :, i] = np.round(xyze[:, :, i], decimals=3)
-            data.bt_vel[:, i] = np.round(bt_xyze[:, i], decimals=3)
-    else:
-        enu = transform.rdi_xyz_enu(
-            xyze, dataset['heading'], dataset['pitch'], dataset['roll_'], orientation=orientation,
-        )
-        bt_enu = transform.rdi_xyz_enu(
-            bt_xyze, dataset['heading'], dataset['pitch'], dataset['roll_'], orientation=orientation,
-        )
-        dataset.attrs["coord_system"] = "earth"
-
-        for i in range(4):
-            data.vel[:, :, i] = np.round(enu[:, :, i], decimals=3)
-            data.bt_vel[:, i] = np.round(bt_enu[:, i], decimals=3)
+    """
+    enu = transform.rdi_xyz_enu(
+        np.stack([dataset[v].T for v in vels], axis=2),
+        dataset['heading'], dataset['pitch'], dataset['roll_'],
+        orientation=dataset.attrs['orientation'],
+    )
+    for i, v in enumerate(vels):
+        dataset[v].values = np.round(enu[:, :, i].T, decimals=3)
 
 
-def reshape_velocity(dataset: xr.Dataset):
-    pass
+
+
+
