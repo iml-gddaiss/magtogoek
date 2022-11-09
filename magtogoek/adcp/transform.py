4d7acb688e08968c345736de777877d674b9bc91
jeromejguay <jerome.guay@protonmail.com>
2022-11-09 13:09:38 -0500

    restructuring the funcitons

--- magtogoek/adcp/transform.py
+++ magtogoek/adcp/transform.py
@@ -61,20 +61,26 @@ def coordsystem2earth(dataset: xr.Dataset) -> xr.Dataset:
 
     Replace velocities variables in the dataset.
 
-    #FIXME NEEDS TESTING
+    #FIXME NEEDS SOME MORE TESTING
 
     Parameters
     ----------
     dataset :
         Required Variables:
-            Velocities: `v1`, `v2`, `v3`, `v4` or `u`, `v`, `w`, `e`
-            `heading`
-            `pitch`
-            `roll_`
+            Velocities: ('v1','v2','v3','v4') or ('u', 'v', 'w', 'e').
+            'heading'
+            'pitch'
+            'roll_'
         Required Attributes:
-            `beam_angle`: int
-            `beam_pattern`: `convex` or `concave`
-            `orientation`: `up` or `down`
+            'beam_angle': int
+            'beam_pattern': 'convex' or 'concave'
+            'orientation': 'up' or 'down'
+    Returns
+    -------
+    dataset with either:
+        beam velocities ('v1','v2','v3','v4') replaced by xyz velocities ('u', 'v', 'w', 'e')
+        or
+        xyz velocities ('u', 'v', 'w', 'e') replaced by enu velocities ('u', 'v', 'w', 'e').
 
     Notes
     -----
@@ -87,133 +93,160 @@ def coordsystem2earth(dataset: xr.Dataset) -> xr.Dataset:
     """
     l.reset()
 
-    beam_vels_name = ('v1', 'v2', 'v3', 'v4')
-    xyze_vels_name = ('u', 'v', 'w', 'e')
-    bt_beam_vels_name = ('bt_' + v for v in beam_vels_name)
-    bt_xyze_vels_name = ('bt_' + v for v in xyze_vels_name)
-
     if dataset.attrs['coord_system'] == "beam" and dataset.attrs['beam_angle'] is not None:
-        compute_beam2xyze(dataset, beam_vels_name, xyze_vels_name)
-        dataset = dataset.drop_vars(beam_vels_name)
+        dataset = beam2xyze(dataset)
 
-        if all(v in dataset for v in bt_beam_vels_name):
-            compute_beam2xyze(dataset, bt_beam_vels_name, bt_xyze_vels_name)
-            dataset = dataset.drop_vars(('bt_' + v for v in beam_vels_name))
-
-        dataset.attrs['coord_system'] = "xyz"
-        l.log('Data transformed from beam to xyze coordinates.')
-
-    if any([(dataset['heading'] == 0).all(), (dataset['roll_'] == 0).all(), (dataset['pitch'] == 0).all()]):
-        l.warning("Roll, Pitch or Heading seems to be missing from the data file. Data not transformed to earth coordinates.")
-    else:
-        xyze2enu(dataset, xyze_vels_name)
-
-        if all(v in dataset for v in bt_xyze_vels_name):
-            xyze2enu(dataset, bt_xyze_vels_name)
-
-        dataset.attrs['coord_system'] = "earth"
-        l.log('Data transformed from xyze to earth coordinates.')
+    if dataset.attrs['coord_system'] == 'xyz':
+        if any([(dataset['heading'] == 0).all(), (dataset['roll_'] == 0).all(), (dataset['pitch'] == 0).all()]):
+            l.warning(
+                "Roll, Pitch or Heading seems to be missing from the data file. Data not transformed to earth coordinates.")
+        else:
+            xyz2enu(dataset)
 
     dataset.attrs["logbook"] += l.logbook
 
     return dataset
 
 
-def compute_beam2xyze(dataset: xr.Dataset, beam_vels_name: Tuple[str] = None, xyze_vels_name: Tuple[str] = None):
-    """Compute xyze velocities from beam velocities.
-
-    Add variables `u`, `v`, `w`, `e` to dataset.
+def beam2xyze(dataset: xr.Dataset) -> xr.Dataset:
+    """Transform xyze velocities from beam velocities.
 
     Parameters
     ----------
     dataset :
+        Required Variables
+            Beam Velocities: ('v1', 'v2', 'v3','v4')
+        Required Coordinates
+            ["depth", "time"]
         Required Attributes:
             `beam_angle`: int
             `beam_pattern`: `convex` or `concave`
-    beam_vels_name : Default `v1`, `v2`, `v3`,`v4`
-        Name of the beam velocities in the dataset.
-    xyze_vels_name : Default `u`, `v`, `w`, `e`
-        Name for the computed xyze velocities.
+    Returns
+    -------
+    dataset with beam velocities ('v1','v2','v3','v4') replaced by xyz ('u', 'v', 'w', 'e').
     """
-    if beam_vels_name is None:
-        beam_vels_name = ('v1', 'v2', 'v3', 'v4')
-    if xyze_vels_name is None:
-        xyze_vels_name = ('u', 'v', 'w', 'e')
-
     trans = transform.Transform(angle=dataset.attrs['beam_angle'], geometry=dataset.attrs['beam_pattern'])
-    xyze = trans.beam_to_xyz(np.stack([dataset[v].T for v in beam_vels_name], axis=2))
 
-    for i, v in enumerate(xyze_vels_name):
-        dataset[v] = (["depth", "time"], np.round(xyze[:, :, i].T, decimals=3))
+    beam_velocities = ('v1', 'v2', 'v3', 'v4')
+    _beam2xyz(dataset, trans, beam_velocities)
+    xyze_velocities = ('u', 'v', 'w', 'e')
+    dataset = dataset.rename({b: v for b, v in zip(beam_velocities, xyze_velocities)})
+    l.log('Water velocities transformed from beam to xyz coordinates.')
+
+    bt_beam_velocities = ('bt_v1', 'bt_v2', 'bt_v3', 'bt_v4')
+    if all(v in dataset for v in bt_beam_velocities):
+        _beam2xyz(dataset, trans, bt_beam_velocities)
+        bt_xyze_velocities = ('bt_u', 'bt_v', 'bt_w', 'bt_e')
+        dataset = dataset.rename({b: v for b, v in zip(bt_beam_velocities, bt_xyze_velocities)})
+        l.log('Bottom velocities transformed from beam to xyz coordinates.')
+
+    dataset.attrs['coord_system'] = "xyz"
+
+    return dataset
 
 
-def compute_xyze2beam(dataset: xr.Dataset, beam_vels_name: Tuple[str] = None, xyze_vels_name: Tuple[str] = None):
-    """Compute beam velocities from xyze velocities.
+def _beam2xyz(dataset: xr.Dataset, trans: transform.Transform, velocities: Tuple[str]):
+    """See beam2xyze
+    """
+    trans_vel = trans.beam_to_xyz(np.stack([dataset[v].T for v in velocities], axis=2))
+    for i, v in enumerate(velocities):
+        dataset[v] = (["depth", "time"], np.round(trans_vel[:, :, i].T, decimals=3))
 
-    Add variables `v1`, `v2`, `v3`, `v4` to dataset.
+
+def xyz2beam(dataset: xr.Dataset) -> xr.Dataset:
+    """Transform beam velocities from xyz velocities.
+
+    Parameters
+    ----------
+    dataset :
+        Required Variables
+            Beam Velocities: ('u', 'v', 'w', 'e')
+        Required Coordinates
+            ["depth", "time"]
+        Required Attributes:
+            `beam_angle`: int
+            `beam_pattern`: `convex` or `concave`
+    Returns
+    -------
+    dataset with xyz ('u', 'v', 'w', 'e') replaced by beam velocities ('v1','v2','v3','v4').
+    """
+    trans = transform.Transform(angle=dataset.attrs['beam_angle'], geometry=dataset.attrs['beam_pattern'])
+
+    xyze_velocities = ('u', 'v', 'w', 'e')
+    _xyz2beam(dataset, trans, xyze_velocities)
+    beam_velocities = ('v1', 'v2', 'v3', 'v4')
+    dataset = dataset.rename({v: b for v, b in zip(xyze_velocities, beam_velocities)})
+    l.log('Water velocities transformed from xyz to beam coordinates.')
+
+    bt_xyze_velocities = ('bt_u', 'bt_v', 'bt_w', 'bt_e')
+    if all(v in dataset for v in bt_xyze_velocities):
+        _xyz2beam(dataset, trans, bt_xyze_velocities)
+        bt_beam_velocities = ('bt_v1', 'bt_v2', 'bt_v3', 'bt_v4')
+        dataset = dataset.rename({v: b for v, b in zip(bt_xyze_velocities, bt_beam_velocities)})
+        l.log('Bottom velocities transformed from xyz to beam coordinates.')
+
+    dataset.attrs['coord_system'] = "beam"
+
+    return dataset
+
+
+def _xyz2beam(dataset: xr.Dataset, trans: transform.Transform, velocities: Tuple[str]):
+    """See beam2xyze
+    """
+    trans_vel = trans.xyz_to_beam(np.stack([dataset[v].T for v in velocities], axis=2))
+    for i, v in enumerate(velocities):
+        dataset[v] = (["depth", "time"], np.round(trans_vel[:, :, i].T, decimals=3))
+
+
+def xyz2enu(dataset: xr.Dataset):
+    """Transform velocities from xyz velocities to enu.
 
     Parameters
     ----------
     dataset :
         Required Variables:
-            xyze_vels_name
+            'u', 'v', 'w', 'e'
+            'heading'
+            'pitch'
+            'roll_'
         Required Attributes:
-            `beam_angle`: int
-            `beam_pattern`: `convex` or `concave`
-    beam_vels_name : Default `v1`, `v2`, `v3`,`v4`
-        Name for the computed beam velocities.
-    xyze_vels_name : Default `u`, `v`, `w`, `e`
-        Name of the xyze velocities in the dataset.
-
+            `orientation`: 'up' or 'down
     """
-    if xyze_vels_name is None:
-        xyze_vels_name = ('u', 'v', 'w', 'e')
-    if beam_vels_name is None:
-        beam_vels_name = ('v1', 'v2', 'v3', 'v4')
+    velocities = ('u', 'v', 'w', 'e')
+    bt_velocities = ('bt_u', 'bt_v', 'bt_w', 'bt_e')
 
-    trans = transform.Transform(angle=dataset.attrs['beam_angle'], geometry=dataset.attrs['beam_pattern'])
-    beam = trans.xyz_to_beam(np.stack([dataset[v].T for v in xyze_vels_name], axis=2))
+    _xyz2enu(dataset, velocities)
+    l.log('Water velocities transformed from xyze to earth coordinates.')
+    if all(v in dataset for v in bt_velocities):
+        _xyz2enu(dataset, bt_velocities)
+        l.log('Bottom velocities transformed from xyze to earth coordinates.')
 
-    for i, v in enumerate(beam_vels_name):
-        dataset[v] = (["depth", "time"], np.round(beam[:, :, i].T, decimals=3))
+    dataset.attrs['coord_system'] = "earth"
 
 
-def xyze2enu(dataset: xr.Dataset, vels_name: Tuple[str]=None):
-    """Transform xyze velocities to enu velocities.
-
-    Parameters
-    ----------
-    dataset :
-        Required Variables:
-            vels_name
-            `heading`
-            `pitch`
-            `roll_`
-        Required Attributes:
-            `orientation`: `up` or `down`
-    vels_name : Default `u`, `v`, `w`, `e`
-        Name of the velocities in the dataset.
+def _xyz2enu(dataset: xr.Dataset, velocities: Tuple[str]):
+    """See xyze2enu
     """
-    if vels_name is None:
-        vels_name = ('u', 'v', 'w', 'e')
     enu = transform.rdi_xyz_enu(
-        np.stack([dataset[v].T for v in vels_name], axis=2),
+        np.stack([dataset[v].T for v in velocities], axis=2),
         dataset['heading'], dataset['pitch'], dataset['roll_'],
         orientation=dataset.attrs['orientation'],
     )
-    for i, v in enumerate(vels_name):
+    for i, v in enumerate(velocities):
         dataset[v].values = np.round(enu[:, :, i].T, decimals=3)
 
 
 if __name__ == "__main__":
-    #import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
     import xarray as xr
+
     #
     # # vels=('LCEWAP01', 'LCNSAP01', 'LRZAAP01', 'LERRAP01')
     # vels = ('u', 'v', 'w', 'e')
     #
     path = "/home/jeromejguay/ImlSpace/Data/Sillex2019/"
-    filename = "D30K_001.nc"
+    #filename = "D30K_001.nc"
+    filename = "D60K_000.nc"
     #
     ds = xr.open_dataset(path + filename)
     # ds = ds.sel(time=slice('2018-10-01T00:00:00', '2018-10-01T01:00:00'))
