2c09ff68f459d6f960b428a4add45a082cada329
jeromejguay <jerome.guay@protonmail.com>
2022-11-17 11:15:51 -0500

    adding bloc=False to adcp plotting.

--- magtogoek/adcp/adcp_plots.py
+++ magtogoek/adcp/adcp_plots.py
@@ -143,7 +143,7 @@ def make_adcp_figure(dataset: xr.Dataset,
 
     if show_fig is True:
         logging.info(f'make adcp_figure show fig: {show_fig}')
-        plt.show()
+        plt.show(block=False)
     else:
         plt.close('all')
 
