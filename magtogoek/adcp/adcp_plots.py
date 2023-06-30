fixing units bug adcp plots

magtogoek/adcp/adcp_plots.py
@@ -233,8 +233,7 @@ def plot_velocity_fields(dataset: xr.Dataset, vel_var: List[str] = ("u", "v", "w
         axe.xaxis_date()
 
         cbar = plt.colorbar(im, location="right", ax=axe, pad=0.01)
-        #cbar.set_label(dataset[var].attrs["units"], fontdict=FONT) #FIXME
-        cbar.set_label('FIXME')
+        cbar.set_label(dataset[var].attrs["units"], fontdict=FONT)
         cbar.set_ticks(np.linspace(-vmax, vmax, 5))
         axe.set_title(var, fontdict=FONT)
         axe.tick_params(rotation=-30)
@@ -289,8 +288,7 @@ def plot_vel_series(dataset: xr.Dataset, depths: Union[float, List[float]],
         clines = cycle(["solid", "dotted", "dashed", "dashdotted"])
         for depth, c in zip(depths, colors):
             ax.plot(dataset.time, da.sel(depth=depth), linestyle=next(clines), c=c, label=f"{depth:.02f} m")
-        #ax.set_ylabel(f"{var}\n[{dataset[var].attrs['units']}]", fontdict=FONT) #FIXME
-        ax.set_ylabel("FIXME")
+        ax.set_ylabel(f"{var}\n[{dataset[var].attrs['units']}]", fontdict=FONT)
     axes[-1].set_xlabel("time", fontdict=FONT)
     axes[2].legend(title="depth")
 
@@ -300,7 +298,7 @@ def plot_vel_series(dataset: xr.Dataset, depths: Union[float, List[float]],
 def plot_pearson_corr(dataset: xr.Dataset, vel_var: List[str] = ("u", "v", "w"), flag_thres: int = 2):
     corr = {v: [] for v in vel_var}
     for var in vel_var:
-        da = flag_data(dataset=dataset, var=var)
+        da = flag_data(dataset=dataset, var=var, flag_thres=flag_thres)
         for d in range(dataset.dims["depth"] - 2):
             if np.isfinite(da[d]).any() and np.isfinite(da[d + 2]).any():
                 corr[var].append(xr.corr(da[d], da[d + 2], "time"))