0277a228bbb4be2c021198e860603b85c0dc27e4
jeromejguay <jerome.guay@protonmail.com>
2024-03-12 12:14:08 -0400

    update winch reader for 2024 mitis winch format

--- magtogoek/meteoce/winch_reader.py
+++ magtogoek/meteoce/winch_reader.py
@@ -1,40 +1,62 @@
+"""
+March 2024
+"""
 from typing import List, Union
 
+from csv import reader
 import pandas as pd
 import xarray as xr
 
 from magtogoek.utils import get_files_from_expression
 
+DATA_START_DELIMITER = "D"
+LINE_SAMPLE_PREFIX = "[S]"
 
-def winch_txt_reader(files: Union[str, List[str]]):
+UNITS_MAP = {
+                'temperature': "degree_C",
+                'conductivity': "S/m",
+                'pressure': "db",
+                'salinity': "PSU",
+                'fluorescence': "micro-g/l",
+                'oxygen': "micromol/l",
+             }
+
+def winch_reader(files: Union[str, List[str]]):
+    """Made for 2024 Mitis Winch Files format.
+    """
     datasets = []
     for fn in get_files_from_expression(files):
         with open(fn, 'r') as f:
-            time = pd.to_datetime(f.readline().strip('\n'), format='%H:%M:%S %y/%m/%d')
-
+            header = []
             while True:
-                l = f.readline()
+                line = f.readline()
 
-                if "D" in l:
+                if line.strip('\n') == DATA_START_DELIMITER:
                     break
+                else:
+                    header.append(line.strip("**"))
 
-            df = pd.read_csv(f,
-                             names=['temperature', 'conductivity', 'depth', 'salinity', 'unknown0', 'unknown1'],
-                             na_values={'unknown1': 'S0001'}
-                             )#, index_col='depth')
+            data = [(v0.strip(LINE_SAMPLE_PREFIX), *values)  for v0, *values in reader(f, delimiter=',') if LINE_SAMPLE_PREFIX in v0]
+
+            df = pd.DataFrame(
+                data,
+                dtype=float,
+                columns=['temperature', 'conductivity', 'pressure', 'salinity', 'fluorescence', 'oxygen'],
+            )
 
             ds = xr.Dataset.from_dataframe(df)
 
-            ds=ds.assign_coords({'time': time})
-   #          ds = xr.Dataset({
-   #              'temperature': (['depth', 'time'], df['temperature']]),
-   #              'conductivity': (['depth', 'time'], df['conductivity'].values]),
-   #              # 'depth': (['depth', 'time'], df['depth']),
-   #              'unknown0': (['depth', 'time'], df['unknown0'].values]),
-   #              'unknown1': (['depth', 'time'], df['unknown1'].values]),
-   #          },
-   # #             coords={'time': time, 'depth': df['depth'].values}
-   #          )
+            for var in set(ds.variables) & set(UNITS_MAP.keys()):
+                ds[var].attrs['units'] = UNITS_MAP[var]
+
+
+            # Adding global attributes from winch files.
+            global_attrs = {}
+            for line in header:
+                k, v = line.replace("\t", "").strip('\n').split(":", 1)
+                global_attrs[k.strip()] = v.strip()
+
+            ds.attrs.update(global_attrs)
 
         datasets.append(ds)
 
@@ -42,4 +64,10 @@ def winch_txt_reader(files: Union[str, List[str]]):
 
 
 if __name__ == "__main__":
-    pass
+    test_files = "/home/jeromejguay/ImlSpace/Data/pmza_2024/IML4-test/winch/"
+
+    files = get_files_from_expression(test_files+"WDATA*.txt")
+
+    dss = winch_reader(files=files)
+
+    ds = dss[0]
