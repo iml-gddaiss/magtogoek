vikig co2 reading indx error fix. pco2 computation updated. (to verify)

magtogoek/metoce/loader.py
@@ -38,11 +38,9 @@ import pint_xarray # pint_xarray modify the xr.Dataset Object
 KNOTS_TO_METER_PER_SECONDS = 0.5144444444 # mps/knots
 MILLIMETER_TO_METER = 1 / 1000 # m / 1000 mmm
 CENTIMETER_TO_METER = 1 / 100  # m / 100 cm
-MILLIBAR_TO_ATMOSPHERE = 1 / 1013.25 # 1 atm / 1013.25 mbar
 
 RTI_FILL_VALUE = 88888
 RDI_FILL_VALUE = -32768.0
-
 
 def load_metoce_data(
         filenames: Union[str, List[str]],
@@ -238,21 +236,15 @@ def _load_viking_metoce_data(viking_data: VikingData) -> Tuple[Dict[str, Tuple[n
         l.log('Par Digi data loaded.')
 
     if viking_data.co2_a is not None:
-        #_pco2_air = viking_data.co2_a['co2_ppm'] * viking_data.co2_a['cell_gas_pressure_mbar'] * MILLIBAR_TO_ATMOSPHERE
         data.update({
-            #'pco2_air': (_pco2_air, {'units': 'uatm'}),
             'co2_air': (viking_data.co2_a['co2_ppm'], {'units': 'ppm'}),
-            'co2_air_pressure': (viking_data.co2_a['cell_gas_pressure_mbar'], {'units': 'mbar'})
         })
         l.log('Co2_a data loaded.')
 
     if viking_data.co2_w is not None:
-        #_pco2_water = viking_data.co2_w['co2_ppm'] * viking_data.co2_w['cell_gas_pressure_mbar'] * MILLIBAR_TO_ATMOSPHERE
         data.update(
             {
-                #'pco2_water': (_pco2_water, {'units': 'uatm'}),
                 'co2_water': (viking_data.co2_w['co2_ppm'], {'units': 'ppm'}),
-                'co2_water_pressure': (viking_data.co2_w['cell_gas_pressure_mbar'], {'units': 'mbar'})
             }
         )
         l.log('Co2_w data loaded.')
@@ -312,8 +304,6 @@ def _load_viking_metoce_data(viking_data: VikingData) -> Tuple[Dict[str, Tuple[n
                 viking_data.rti[_name][viking_data.rti[_name] == RTI_FILL_VALUE] = np.nan
                 data[_name] = (viking_data.rti[_name] * MILLIMETER_TO_METER, {"units": "m/s"}) #rounded to mm.
             l.log('Rti data loaded.')
-
-    #data = _fill_data(data)
 
     return data, global_attrs
 
@@ -426,12 +416,11 @@ def _load_metis_metoce_data(metis_data: MetisData) -> Tuple[Dict[str, Tuple[np.m
 
 
     if metis_data.pco2 is not None:
-        _pco2_air = metis_data.pco2['co2_ppm_air'] * metis_data.pco2['gas_pressure_air_mbar'] * MILLIBAR_TO_ATMOSPHERE
-        _pco2_water = metis_data.pco2['co2_ppm_water'] * metis_data.pco2['gas_pressure_water_mbar'] * MILLIBAR_TO_ATMOSPHERE
+
         data.update(
             {
-                'pco2_air': (_pco2_air, {'units': 'uatm'}),
-                'pco2_water': (_pco2_water, {'units': 'uatm'})
+                'co2_air': (metis_data.pco2['co2_ppm_air'], {'units': 'ppm'}),
+                'co2_water': (metis_data.pco2['co2_ppm_water'], {'units': 'ppm'}),
              }
         )
         l.log('PCO2 Data Loaded')
@@ -449,7 +438,7 @@ def _average_duplicates(dataset: xr.Dataset, coord: str) -> xr.Dataset:
     """Average data_array values of duplicates time coords index.
     """
 
-    # average any duplicate value along coord by groupping according to coord value.
+    # average any duplicate value along coord by grouping according to coord value.
     df = dataset.to_dataframe()
     df = df.groupby(coord).mean(numeric_only=False)
 

magtogoek/wps/sci_tools.py
@@ -16,7 +16,7 @@ import numpy as np
 import gsw
 from typing import Union, List
 
-CELCIUS_TO_KELVIN = 273.15
+CELSIUS_TO_KELVIN = 273.15
 
 GAS_CONSTANT = 8.31446261815324  # the constant in seabird docs differs 8.3144621 J/(K mol). Application Note 99
 FARADAY_CONSTANT = 96485.365
@@ -266,7 +266,7 @@ def _compute_scaled_temperature(temperature: Union[float, np.ndarray]) -> Union[
     -------
 
     """
-    return np.log((298.15 - temperature) / (CELCIUS_TO_KELVIN + temperature))
+    return np.log((298.15 - temperature) / (CELSIUS_TO_KELVIN + temperature))
 
 
 def phINT_from_voltINT(temp: Union[float, np.ndarray], volt: Union[float, np.ndarray], k0: float, k2: float) -> Union[float, np.ndarray]:
@@ -445,7 +445,7 @@ def pHEXT_from_voltEXT(volt: Union[float, np.ndarray], temp: Union[float, np.nda
 
 
 def _compute_s_nernst(temp:  Union[float, np.ndarray]) -> Union[float, np.ndarray]:
-    return GAS_CONSTANT * (temp + CELCIUS_TO_KELVIN) * np.log(10) / FARADAY_CONSTANT
+    return GAS_CONSTANT * (temp + CELSIUS_TO_KELVIN) * np.log(10) / FARADAY_CONSTANT
 
 
 def total_chloride_in_seawater(psal: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
@@ -605,7 +605,7 @@ def acid_dissociation_constant_HSO4(temp: Union[float, np.ndarray], psal: Union[
 
     .. [2] Sea-Bird Scientific, Technical Note on Calculating pH, Application Note 99
     """
-    temp_k = temp + CELCIUS_TO_KELVIN
+    temp_k = temp + CELSIUS_TO_KELVIN
     ionic_s = sample_ionic_strength(psal=psal)
     a0 = (-4276.1 / temp_k) + 141.328 - 23.093 * np.log(temp_k)
     a1 = ((-13856 / temp_k) + 324.57 - 47.986 * np.log(temp_k)) * np.sqrt(ionic_s)
@@ -638,7 +638,7 @@ def log_of_HCl_activity_as_temperature_and_pressure_function(psal: Union[float, 
    pres :
        pressure in dbar
    """
-    temp_k = temp + CELCIUS_TO_KELVIN
+    temp_k = temp + CELSIUS_TO_KELVIN
     log_t = log_of_HCl_activity_as_temperature_function(psal=psal, temp=temp)
     v_hcl = partial_molal_volume_hcl(temp=temp)
 
@@ -825,37 +825,69 @@ def compute_pco2_air(co2: np.ndarray, pressure: np.ndarray) -> np.ndarray:
     """
     return co2 * pressure
 
-def compute_pco2_water(co2_water: np.array, salinity: np.ndarray, temperature: np.ndarray) -> np.ndarray:
+def compute_pco2_water(xco2: np.array, salinity: np.ndarray, temperature: np.ndarray, pressure: np.ndarray) -> np.ndarray:
     """
 
-    ```[Thermodynamics of the carbon dioxide system in the oceans. Frank J. Millero, 1994, Pergamon]
-    a0 = -60.2409 + 93.4517 * (100/temperature) + 23.3585 * ln(temperature/100)
-    a1 = salinity * [0.023517-0.023656 * (temperature/100) + 0.0047036 * (temperature/100)^2]
-
-    k0 = exp(a0 + a1)
-
-    pCO2_water = CO2_water[ppm] / (k0[mol*kg-1*atm-1] * mmCO2[kg/mol])
-
-    * temperate in kelvin
-    * k0 units are [mol*kg-1*atm-1]
-    * mmCO2: co2 molar mass = 0.044 [kg/mol]
-    ```
 
     Parameters
     ----------
-    co2_water
-    salinity
-    temperature
+    xco2:
+        [ppm]
+    salinity:
+        [PSU]
+    temperature:
+        seawater temperature [celsius[
+    pressure:
+        atmospheric [atm]
 
     Returns
     -------
     pco2_water
 
     """
-    temperature += CELCIUS_TO_KELVIN
-    a0 = -60.2409 + 93.4517 * (100 / temperature) + 23.3585 * np.log(temperature / 100)
-    a1 = salinity * (0.023517 - 0.023656 * (temperature / 100) + 0.0047036 * (temperature / 100) ** 2)
 
-    k0 = np.exp(a0 + a1)
+    # this seems to be the fugacity factor
+    # temperature_K = temperature + CELSIUS_TO_KELVIN
+    # lnk0 = (
+    #         -60.2409
+    #         + 93.4517 * (100 / temperature_K)
+    #         + 23.3585 * np.log(temperature_K / 100)
+    #         + salinity * (0.023517 - 0.023656 * (temperature_K / 100) + 0.0047036 * (temperature_K / 100) ** 2)
+    # )
+    #k0 = np.exp(lnk0)
 
-    return co2_water / (k0 * CO2_MOLAR_MASS)
+    kvp = compute_co2_vapor_pressure_factor(salinity=salinity, temperature=temperature, pressure=pressure)
+
+    return xco2 * kvp
+
+
+
+def compute_co2_vapor_pressure_factor(salinity: np.ndarray, temperature: np.ndarray, pressure: np.ndarray) -> np.ndarray:
+    """
+
+    Parameters
+    ----------
+    salinity:
+        [PSU]
+    temperature:
+        seawater temperature [celsius[
+    pressure:
+        atmospheric [atm]
+
+    Returns
+    -------
+        vapor pressure factor
+
+    Source
+    ------
+    https://github.com/mvdh7/PyCO2SYS/blob/2705ff00ca5ddd47d0d7c905b9138d8ee3785563/PyCO2SYS/gas.py#L67
+
+    """
+    temperature_K = temperature + CELSIUS_TO_KELVIN
+    lnkvp = (
+        24.4543 - 67.4509 * (100 / temperature_K)
+        - 4.8489 * np.log(temperature_K/ 100)
+        - 0.000544 * salinity
+    )
+
+    return pressure - np.exp(lnkvp)